6bc3dc9381ae2c3a427875992e64fdab42cdbf2c
Sofia <a.sofia00@hotmail.com>
2019-05-08 16:22:27 +0100

    remove variavel

--- Aluguer.java
+++ Aluguer.java
@@ -7,8 +7,6 @@ public class Aluguer
     private Veiculo veiculo;
     //Cliente que realizou o aluguer
     private Cliente cliente;
-    //Proprieatrio do Carro
-    private Proprietario proprietario;
     //Viagem que o carro fez no aluguer
     private Viagem viagemCarro;
     //Viagem que o Cliente fez ate ao carro
@@ -23,7 +21,6 @@ public class Aluguer
     public Aluguer(){
         this.veiculo = new Veiculo();
         this.cliente = new Cliente();
-        this.proprietario = new Proprietario();
         this.viagemCarro = new Viagem();
         this.viagemCliente = new Viagem();
         this.custoTotal = 0;
@@ -36,7 +33,6 @@ public class Aluguer
     public Aluguer(Aluguer a) {
         this.veiculo = a.getVeiculo();
         this.cliente = a.getCliente();
-        this.proprietario = a.getProprietario();
         this.viagemCarro = a.getViagemCarro();
         this.viagemCliente = a.getViagemCliente();
         this.custoTotal = a.getCustoTotal();
@@ -45,15 +41,13 @@ public class Aluguer
      * Construtor por parametro
      * @param veiculo
      * @param cliente
-     * @param proprietario
      * @param viagemCarro
      * @param viagemCliente
      * @param custoTotal
      */
-    public Aluguer(Veiculo veiculo, Cliente cliente, Proprietario proprietario, Viagem viagemCarro, Viagem viagemCliente, double custoTotal) {
+    public Aluguer(Veiculo veiculo, Cliente cliente, Viagem viagemCarro, Viagem viagemCliente, double custoTotal) {
         this.veiculo = veiculo;
         this.cliente = cliente;
-        this.proprietario = proprietario;
         this.viagemCarro = viagemCarro;
         this.viagemCliente = viagemCliente;
         this.custoTotal = custoTotal;
@@ -75,13 +69,7 @@ public class Aluguer
         this.cliente = cliente;
     }
 
-    public Proprietario getProprietario() {
-        return proprietario;
-    }
-
-    public void setProprietario(Proprietario proprietario) {
-        this.proprietario = proprietario;
-    }
+ 
 
     public Viagem getViagemCarro() {
         return viagemCarro;
@@ -119,7 +107,7 @@ public class Aluguer
         }
         Aluguer a = (Aluguer) obj;
         return (a.getVeiculo().equals(this.veiculo) && a.getCliente().equals(this.cliente) &&
-                a.getProprietario().equals(this.proprietario) && a.getViagemCarro().equals(this.viagemCarro)
+                 a.getViagemCarro().equals(this.viagemCarro)
                     && a.getCliente().equals(this.viagemCliente) && a.getCustoTotal() == this.custoTotal);
     }
 
@@ -139,7 +127,6 @@ public class Aluguer
         StringBuilder sb = new StringBuilder();
         sb.append("Veiculo:").append(veiculo).append("'\n");
         sb.append("Cliente:").append(cliente).append("'\n");
-        sb.append("Proprietario:").append(proprietario).append("'\n");
         sb.append("Viagem do Carro:").append(viagemCarro).append("'\n");
         sb.append("Viagem do Cliente:").append(viagemCliente).append("'\n");
         return sb.toString();
