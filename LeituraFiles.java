17d1e3dbc00d049b84297556be671141e96913f7
luistrofa2 <47892355+luistrofa2@users.noreply.github.com>
2019-05-14 16:15:24 +0100

    Add files via upload
    leitura do log para introduçao do novocarro

---
+++ LeituraFiles.java
@@ -0,0 +1,57 @@
+import java.awt.geom.Point2D;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeituraFiles {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File ficheiro = new File("src/logsPOO_carregamentoInicial.bak");
+        FileReader fr = new FileReader(ficheiro);
+        String[] campos;
+        String[] camposaux;
+
+        List<Veiculo> lv = new ArrayList<>();
+
+        try {
+            BufferedReader inStream = new BufferedReader(fr);
+            String st;
+            while ((st = inStream.readLine()) != null) {
+                campos = st.split(":");
+                if (campos[0].equals("NovoCarro")){
+                    camposaux = campos[1].split(",");
+                    Veiculo v = new Veiculo();
+                    //v.setTipo(TipoVeiculo.camposaux[0]);
+                    v.setMarca(camposaux[1]);
+                    v.setMatricula(camposaux[2]);
+                    v.setNif(Integer.parseInt(camposaux[3]));
+                    v.setVelocidadeKm(Double.parseDouble(camposaux[4]));
+                    v.setPrecoBaseKm(Double.parseDouble(camposaux[5]));
+                    v.setConsumoKm(Double.parseDouble(camposaux[6]));
+                    v.setAutonomiaMaxima(Double.parseDouble(camposaux[7]));
+                    v.setAutonomiaAtual(Double.parseDouble(camposaux[7]));
+                    //v.setLocalizacao(new Point2D(Double.parseDouble(camposaux[8]), Double.parseDouble(camposaux[9]));
+                    System.out.println(camposaux[0]); //tipo
+                    System.out.println(camposaux[1]); //marca
+                    System.out.println(camposaux[2]); //matricula
+                    System.out.println(camposaux[3]); //nif
+                    System.out.println(camposaux[4]); //velocidade media
+                    System.out.println(camposaux[5]); //preço por km
+                    System.out.println(camposaux[6]); //consumo por km
+                    System.out.println(camposaux[7]); //autonomia
+                    System.out.println(camposaux[8]); //X
+                    System.out.println(camposaux[9]); //Y
+                lv.add(v);
+                }
+            }
+            for (Veiculo v : lv){
+                System.out.println(v);
+            }
+            System.out.println(lv);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+    }
+}
--- Veiculo.java
+++ Veiculo.java
@@ -5,8 +5,14 @@ import java.util.Set;
 public class Veiculo
 {
     /** Variaveis de Instância */
+    //Marca do Carro
+    private String marca;
     //Tipo do veiculo(Gasolina,eletrico ou hibrido)
     private TipoVeiculo tipo;
+    //Matricula do veiculo
+    private String matricula;
+    //Nif do Proprietário
+    private int nif;
     //velocidade média do carro por KIlometro
     private double velocidadeKm;
     //Preço a pagar por cada kilometro
@@ -26,7 +32,7 @@ public class Veiculo
     //Identifica se o carro encontra-se disponivel para um novo aluguer
     private boolean disponibilidade;
     //Proprietario do carro
-    private Proprietario proprietario;
+    //private Proprietario proprietario;
 
     /**
      * Cria uma instância de cliente
@@ -34,7 +40,10 @@ public class Veiculo
      */
     public Veiculo(){
         super();
+        this.marca = " ";
         this.tipo = TipoVeiculo.Gasolina;
+        this.matricula = " ";
+        this.nif = 0;
         this.velocidadeKm = 0;
         this.precoBaseKm = 0;
         this.consumoKm = 0;
@@ -43,8 +52,8 @@ public class Veiculo
         this.localizacao = new Point2D.Double(0,0); //iniciliaza nas coordenadas (0,0)
         this.autonomiaMaxima = 0;
         this.autonomiaAtual = 0;
-        this.disponibilidade = false;
-        this.proprietario = new Proprietario();
+        this.disponibilidade = true;
+      //  this.proprietario = new Proprietario();
     }
 
     /**
@@ -53,7 +62,10 @@ public class Veiculo
      */
     public Veiculo(Veiculo v){
         super();
+        this.marca = v.getMarca();
         this.tipo = v.getTipo();
+        this.matricula = v.getMatricula();
+        this.nif = v.getNif();
         this.velocidadeKm = v.getVelocidadeKm();
         this.precoBaseKm = v.getPrecoBaseKm();
         this.consumoKm = v.getConsumoKm();
@@ -63,7 +75,7 @@ public class Veiculo
         this.autonomiaMaxima = v.getAutonomiaMaxima();
         this.autonomiaAtual = v.getAutonomiaAtual();
         this.disponibilidade = v.getDisponibilidade();
-        this.proprietario = v.getProprietario();
+      //  this.proprietario = v.getProprietario();
 
     }
 
@@ -79,10 +91,12 @@ public class Veiculo
      * @param autonomiaMaxima
      * @param autonomiaAtual
      * @param disponibilidade
-     * @param prop
      */
-    public Veiculo(TipoVeiculo tipo, double velocidadeKm, double precoBaseKm, double consumoKm, Set<Aluguer> alugueresRealizados, int classificacao, Point2D localizacao, double autonomiaMaxima, double autonomiaAtual, boolean disponibilidade, Proprietario proprietario) {
+    public Veiculo(String marca,TipoVeiculo tipo, String matricula, int nif, double velocidadeKm, double precoBaseKm, double consumoKm, Set<Aluguer> alugueresRealizados, int classificacao, Point2D localizacao, double autonomiaMaxima, double autonomiaAtual, boolean disponibilidade) {
+        this.marca = marca;
         this.tipo = tipo;
+        this.matricula = matricula;
+        this.nif = nif;
         this.velocidadeKm = velocidadeKm;
         this.precoBaseKm = precoBaseKm;
         this.consumoKm = consumoKm;
@@ -92,7 +106,14 @@ public class Veiculo
         this.autonomiaMaxima = autonomiaMaxima;
         this.autonomiaAtual = autonomiaAtual;
         this.disponibilidade = disponibilidade;
-        this.proprietario = proprietario;
+       // this.proprietario = proprietario;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String m){
+        this.marca = m;
     }
 
     public TipoVeiculo getTipo() {
@@ -101,6 +122,22 @@ public class Veiculo
 
     public void setTipo(TipoVeiculo tipo) {
         this.tipo = tipo;
+    }
+
+    public void setMatricula(String m){
+        this.matricula = m;
+    }
+
+    public String getMatricula(){
+        return this.matricula;
+    }
+
+    public void setNif(int n){
+        this.nif = n;
+    }
+
+    public int getNif(){
+        return this.nif;
     }
 
     public double getVelocidadeKm() {
@@ -172,7 +209,7 @@ public class Veiculo
     public void setDisponibilidade(boolean disponibilidade) {
         this.disponibilidade = disponibilidade;
     }
-
+/*
     public Proprietario getProprietario() {
         return this.proprietario;
     }
@@ -180,7 +217,7 @@ public class Veiculo
     public void setProprietario(Proprietario proprietario) {
         this.proprietario = proprietario;
     }
-
+*/
     /**
      * Devolve uma cópia desta instância
      * @return
@@ -200,11 +237,11 @@ public class Veiculo
         if (!super.equals(object)) return false;
 
         Veiculo v = (Veiculo) object;
-        return (this.alugueresRealizados.equals(v.getAlugueresRealizados()) && 
+        return (this.alugueresRealizados.equals(v.getAlugueresRealizados()) && this.matricula.equals(v.getMatricula()) && this.nif == v.getNif() &&
                 this.velocidadeKm == v.getVelocidadeKm() && this.precoBaseKm == v.getPrecoBaseKm() && this.consumoKm == v.getConsumoKm() &&
-                this.localizacao.equals(v.getLocalizacao()) && this.autonomiaAtual == v.getAutonomiaAtual() && 
-                this.autonomiaMaxima == v.getAutonomiaMaxima() && this.disponibilidade == v.getDisponibilidade() && 
-                this.proprietario.equals(proprietario) && this.tipo == v.getTipo() && this.classificacao == v.getClassificacao());
+                this.localizacao.equals(v.getLocalizacao()) && this.autonomiaAtual == v.getAutonomiaAtual() && this.marca.equals(v.getMarca()) &&
+                this.autonomiaMaxima == v.getAutonomiaMaxima() && this.disponibilidade == v.getDisponibilidade()
+                /*this.proprietario.equals(proprietario)*/ && this.tipo == v.getTipo() && this.classificacao == v.getClassificacao());
     }
 
     /**
@@ -213,7 +250,10 @@ public class Veiculo
      */
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        sb.append("Marca:").append(marca).append("'\n");
         sb.append("Tipo:").append(tipo).append("'\n");
+        sb.append("Matricula:").append(matricula).append("'\n");
+        sb.append("Nif:").append(nif).append("'\n");
         sb.append("Velocidade media:").append(velocidadeKm).append("'\n");
         sb.append("Preco por Kilometro:").append(precoBaseKm).append("'\n");
         sb.append("Consumo do carro por Kilometro:").append(consumoKm).append("'\n");
@@ -223,7 +263,7 @@ public class Veiculo
         sb.append("Autonomia Maxima:").append(autonomiaMaxima).append("'\n");
         sb.append("Autonomia Atual:").append(autonomiaAtual).append("'\n");
         sb.append("Disponibilidade do carro:").append(disponibilidade).append("'\n");
-        sb.append("Proprietario do Carro:").append(proprietario).append("'\n");
+       // sb.append("Proprietario do Carro:").append(proprietario).append("'\n");
         return sb.toString();
     }
 }
